37f9412f893669c1e1e60d976d57cca328e6838d
saroarlene <saroarlene@gmail.com>
2023-11-01 15:16:36 +0200

    First React-App

---
+++ react-app/src/App.tsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
+  return (
+    <div>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
+      )}
+      <Button color="secondary" onClick={() => setAlertVisibility(true)}>
+        My Button
+      </Button>
+    </div>
+  );
+}
+
+export default App;
+
+//  import Message from './Message';
+// import Alert from "./components/Alert";
+// import ListGroup from "./components/ListGroup";
+
+//   let items = ["New york", "kigali", "San Francisco", "Rwamagana"];
+
+/* <Alert>
+        Hello <span>World</span>
+      </Alert>
+      <Alert text="Hello World" /> */
+
+//   const handleSelectItem = (item: string) => {
+//     console.log(item);
+//   };
+//   return (
+//     <div>
+//       <ListGroup
+//         items={items}
+//         heading="cities"
+//         onSelectItem={handleSelectItem}
+//       />
+//     </div>
+
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <div className="App">
+//       <div>
+//         <a href="https://vitejs.dev" target="_blank">
+//           <img src="/vite.svg" className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://reactjs.org" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </div>
+//   )
+// }
+
+// export default App
